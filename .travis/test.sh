079fa2fc50b928a107e7f2e7a8d156e2d723a0aa
Dmitry Korotych <dkorotych@gmail.com>
2017-09-25 15:23:51 +0300

    Permission tests

---
+++ .travis/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+function lazybones() {
+    mounts=""
+    for volume in "${HOME}/.lazybones" "${HOME}/.groovy" "${HOME}/.m2"
+    do
+        if [ ! -d "$volume" ]; then
+            mkdir -p ${volume}
+        fi
+        mounts="${mounts} --volume $volume:$volume"
+    done
+    docker run \
+        --rm \
+        --interactive \
+        --tty \
+        --tmpfs /tmp \
+        --volume /etc/group:/etc/group:ro \
+        --volume /etc/passwd:/etc/passwd:ro \
+        --user "$(id -u):$(id -g)" \
+        --volume "${PWD}":/app \
+        ${mounts} \
+    ${TAG} $@
+}
+
+lazybones config add bintrayRepositories dkorotych/lazybones-templates
+mkdir tmp
+cd tmp
+lazybones create maven-quickstart .
+
+stat -c "%U" ~/.lazybones/managed-config.json | grep "$(whoami)"
+stat -c "%U" ~/.lazybones/templates  | grep "$(whoami)"
+stat -c "%U" ~/.groovy/grapes/uk.co.cacoethes/ | grep "$(whoami)"
